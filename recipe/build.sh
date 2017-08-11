ee4a1bfe0ea5e5d9ecd3988b5192e8ab332485d6
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-08-11 20:49:44 +0000

    Make recipe cross-compile capable

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # gstreamer expects libffi to be in lib64 for some reason.
-mkdir $PREFIX/lib64
+mkdir -p $PREFIX/lib64
 cp -r $PREFIX/lib/libffi* $PREFIX/lib64
 
 # The datarootdir option places the docs into a temp folder that won't
