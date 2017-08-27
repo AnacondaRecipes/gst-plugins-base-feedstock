d9caf48f4dca8d2305a99753f41f7ef4ac39860c
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 17:26:33 +0100

    Generalize logging

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cp -r $PREFIX/lib/libffi* $PREFIX/lib64
 # The datarootdir option places the docs into a temp folder that won't
 # be included in the package (it is about 12MB).
 ./configure --disable-examples --prefix="$PREFIX" --datarootdir=`pwd`/tmpshare
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 # Some tests fail because not all plugins are built and it seems
 # tests expect all plugins
 # See this link for an explanation:
